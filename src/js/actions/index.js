fc03d8f7b415bd677b7ed97c0573352e0bc119af
Arthur NESPOULOUS <anespoul@e3r9p2.42.fr>
2019-07-01 08:53:44 +0200

    ajout de react-saga

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -6,12 +6,21 @@ export function addArticle(payload) {
 export function foundBadWord(payload) {
     return { type: FOUND_BAD_WORD, payload } 
 };
+
+// Redux-thunk Example :
+
+// export function getData() {
+//     return function(dispatch) {
+//         return fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(response => response.json())
+//         .then(json => {
+//             dispatch ({ type: "DATA_LOADED", payload:json });
+//         });
+//     }
+// }
+
+// Redux-saga Example :
+
 export function getData() {
-    return function(dispatch) {
-        return fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => {
-            dispatch ({ type: "DATA_LOADED", payload:json });
-        });
-    }
+    return { type: "DATA_REQUESTED" };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
     module: {
         rules: [
             {
